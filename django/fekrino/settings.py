da55421b56179551d1d927c5eb97deb11ed9f4d0
mohsen <mohsen@myhost.local>
2020-04-03 10:33:52 +0430

    Dockerfile updated

--- django/fekrino/settings.py
+++ django/fekrino/settings.py
@@ -209,7 +209,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 CELERY_BROKER_URL = 'amqp://localhost'
