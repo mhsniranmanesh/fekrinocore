07eb29f069fe66cd71ffe1c5d9ac1f9617db7a18
mohsen <mohsen@myhost.local>
2020-04-04 15:23:40 +0430

    Dockerfile updated

--- django/docker-entrypoint.sh
+++ django/docker-entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-python3 manage.py collectstatic
-python3 manage.py makemigrations
-python3 manage.py migrate
+python3 manage.py collectstatic --noinput
+python3 manage.py makemigrations --noinput
+python3 manage.py migrate --noinput
 
-uwsgi --show-config
+uwsgi --show-config --noinput
