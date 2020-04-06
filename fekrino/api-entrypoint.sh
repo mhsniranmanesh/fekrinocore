5c3357e3d74741c3b536f5e1ec5771f568206862
mohsen <mohsen@myhost.local>
2020-04-06 05:34:51 +0430

    logs added to project

--- fekrino/api-entrypoint.sh
+++ fekrino/api-entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-uwsgi --show-config
 python3 manage.py collectstatic --noinput
 python3 manage.py makemigrations --noinput
 python3 manage.py migrate --noinput
-echo "FUCKKKKK YOU"
+
+uwsgi --show-config
