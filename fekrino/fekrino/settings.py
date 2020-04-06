3145a41530c435a2efd96fbbb5bb210956520638
mohsen <mohsen@myhost.local>
2020-04-06 11:45:34 +0430

    celery setting changed

--- fekrino/fekrino/settings.py
+++ fekrino/fekrino/settings.py
@@ -55,7 +55,7 @@ if IS_IN_PRODUCTION is True:
             'PORT': '5432'
         }
     }
-    CELERY_BROKER_URL = 'pyamqp://rabbitmq:5672'
+    CELERY_BROKER_URL = 'redis://redis:6379'
 
 else:
     DEBUG = True
